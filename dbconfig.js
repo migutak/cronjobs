da88894a8fa0571d45b74e53f1930a3cc1c67937
Kevin Miguta <migutak@gmail.com>
2021-05-30 11:50:12 +0300

    adding to queue

--- dbconfig.js
+++ dbconfig.js
@@ -1,6 +1,8 @@
 module.exports = {
   user: process.env.DB_USER || "ecol",
-  password: process.env.DB_PASSWORD || 'L#TTc011',
-  connectString: process.env.DB_CONNECTIONSTRING || "copkprdcont3-scan.co-opbank.co.ke:1559/ECOLLECT",
-  servicename: process.env.SERVICENAME || "Letters"
+  password: process.env.DB_PASSWORD || "ecol",//'L#TTc011',
+  connectString: process.env.DB_CONNECTIONSTRING || "52.117.54.217:1521/ORCLCDB.localdomain",//"copkprdcont3-scan.co-opbank.co.ke:1559/ECOLLECT",
+  servicename: process.env.SERVICENAME || "Letters",
+  //RABBITMQ         : process.env.RABBITMQ || 'amqp://guest:guest@ecollectweb.co-opbank.co.ke',
+  RABBITMQ         : process.env.RABBITMQ || 'amqp://guest:guest@127.0.0.1'
 };
--- index_manual.js
+++ index_manual.js
@@ -1,5 +1,7 @@
 const cron = require('node-cron');
 var oracledb = require('oracledb');
+var validator = require("email-validator");
+var amqp = require('amqplib/callback_api');
 oracledb.outFormat = oracledb.OBJECT;
 oracledb.autoCommit = true;
 var dbConfig = require('./dbconfig.js');
@@ -12,53 +14,95 @@ function currentDate() {
   return [year, month, day].join('-')
 };
 currentDate();
-// var task = cron.schedule('0 7 * * 1-6', () => {
-  // update self cure
- (async function() {
-        try {
-          connection = await oracledb.getConnection({
-            user: dbConfig.user,
-            password: dbConfig.password,
-            connectString: dbConfig.connectString
-          });
-          
-          cureresult = await connection.execute("update demandsdue set status = 'self cure' where status = 'pending' and accnumber not in (select accnumber from loans_stage)");
-          console.log('-> self cure update :' + new Date());      
-          result = await connection.execute("select * from autoletters where active = 'true'");
-          for (i=0; i<result.rows.length; i++) {
-              const memogrp = result.rows[i].MEMOGROUP;
-              const daysinarr = result.rows[i].DAYSINARR;
-              const letterid = result.rows[i].LETTERID;
-              
-              const sqlInsert = "select accnumber,custnumber,nvl(client_name,0) client_name, section, oustbalance, instamount totalarrears, daysinarr, nvl(addressline1,0) address, postcode postalcode, telnumber, emailaddress, colofficer, branchcode, rrocode, arocode, '"+letterid+"' demand from tqall where substr(accnumber,3,3) = '" + memogrp + "' and daysinarr = " + daysinarr;
-              selectResult = await connection.execute(sqlInsert);
-                //console.log('-> total_letters are ' + i);
-                for (x=0; x<selectResult.rows.length; x++) {
-                  // console.log('insert starts ....');
-                  var insertSQL = "insert into demandsdue(accnumber, custnumber, client_name, section, oustbalance, totalarrears, daysinarr, address, postalcode, telnumber, emailaddress, datedue, colofficer, branchcode, rrocode, arocode, demandletter, status) "+ 
-                  " values('" + selectResult.rows[x].ACCNUMBER + "','"+selectResult.rows[x].CUSTNUMBER+"','"+(selectResult.rows[x].CLIENT_NAME).replace(/'/g,'')+"','"+selectResult.rows[x].SECTION+"','"+selectResult.rows[x].OUSTBALANCE+"','"+selectResult.rows[x].TOTALARREARS+"','"+selectResult.rows[x].DAYSINARR+"','"+(selectResult.rows[x].ADDRESS).replace(/'/g,'')+"','"+selectResult.rows[x].POSTALCODE+"','"+selectResult.rows[x].TELNUMBER+"','"+selectResult.rows[x].EMAILADDRESS+"','"+currentDate()+"','"+selectResult.rows[x].COLOFFICER+"','"+selectResult.rows[x].BRANCHCODE+"','"+selectResult.rows[x].RROCODE+"','"+selectResult.rows[x].AROCODE+"','"+selectResult.rows[x].DEMAND+"', 'pending')"
-                  //console.log(insertSQL);
-                  insertSQLrs = await connection.execute(insertSQL);  
-                  //console.log('insert end ....' + x);
-                };
-          };
-          console.log('Demandsdue insert completed on :' + new Date());
-          console.log('----End----');
-        } catch (err) {
-          console.error(err);
-          console.log('----End----');
-        } finally {
-          if (connection) {
-            try {
-              await connection.close();   // Always close connections
-            } catch (err) {
-              console.error(err.message);
-              console.log('----End----');
-            }
-          }
-        }
-      })();
-// });
+// var task = cron.schedule('0 7 * * 1-6', () => {});
+// update self cure
+(async function () {
+  try {
+    connection = await oracledb.getConnection({
+      user: dbConfig.user,
+      password: dbConfig.password,
+      connectString: dbConfig.connectString
+    });
 
-console.log('[' + dbConfig.servicename + '] cronjob service started at '  + new Date());
+    cureresult = await connection.execute("update demandsdue set status = 'self cure' where status = 'pending' and accnumber not in (select accnumber from loans_stage)");
+    console.log('-> self cure update :' + new Date());
+    result = await connection.execute("select * from autoletters where active = 'true'");
+    for (i = 0; i < result.rows.length; i++) {
+      const memogrp = result.rows[i].MEMOGROUP;
+      const daysinarr = result.rows[i].DAYSINARR;
+      const letterid = result.rows[i].LETTERID;
+
+      const sqlInsert = "select accnumber,custnumber,nvl(client_name,0) client_name, section, oustbalance, instamount totalarrears, daysinarr, nvl(addressline1,0) address, postcode postalcode, telnumber, emailaddress, colofficer, branchcode, rrocode, arocode, '" + letterid + "' demand from tqall where substr(accnumber,3,3) = '" + memogrp + "' and daysinarr = " + daysinarr;
+      selectResult = await connection.execute(sqlInsert);
+      for (x = 0; x < selectResult.rows.length; x++) {
+        var status = 'pending';
+        if (validator.validate(selectResult.rows[x].EMAILADDRESS)) {
+          var insertSQL = "insert into demandsdue(accnumber, custnumber, client_name, section, oustbalance, totalarrears, daysinarr, address, postalcode, telnumber, emailaddress, datedue, colofficer, branchcode, rrocode, arocode, demandletter, status) " +
+            " values('" + selectResult.rows[x].ACCNUMBER + "','" + selectResult.rows[x].CUSTNUMBER + "','" + (selectResult.rows[x].CLIENT_NAME).replace(/'/g, '') + "','" + selectResult.rows[x].SECTION + "','" + selectResult.rows[x].OUSTBALANCE + "','" + selectResult.rows[x].TOTALARREARS + "','" + selectResult.rows[x].DAYSINARR + "','" + (selectResult.rows[x].ADDRESS).replace(/'/g, '') + "','" + selectResult.rows[x].POSTALCODE + "','" + selectResult.rows[x].TELNUMBER + "','" + selectResult.rows[x].EMAILADDRESS + "','" + currentDate() + "','" + selectResult.rows[x].COLOFFICER + "','" + selectResult.rows[x].BRANCHCODE + "','" + selectResult.rows[x].RROCODE + "','" + selectResult.rows[x].AROCODE + "','" + selectResult.rows[x].DEMAND + "','" + status + "')"
+
+          insertSQLrs = await connection.execute(insertSQL);
+
+          // push to queue
+          amqp.connect(dbConfig.RABBITMQ, (err, conn) => {
+            if (err != null) bail(err);
+            conn.createChannel(on_open);
+            function on_open(err, ch) {
+              if (err != null) bail(err);
+              var queue = 'pending';
+              ch.assertQueue(queue, { durable: false });
+              ch.sendToQueue(queue, Buffer.from(JSON.stringify(selectResult.rows[x].ACCNUMBER)));
+              console.log('==entry=added=to=>pending');
+            }
+          });
+
+        } else {
+          status = 'invalid emailaddress';
+          var insertSQL = "insert into demandsdue(accnumber, custnumber, client_name, section, oustbalance, totalarrears, daysinarr, address, postalcode, telnumber, emailaddress, datedue, colofficer, branchcode, rrocode, arocode, demandletter, status) " +
+            " values('" + selectResult.rows[x].ACCNUMBER + "','" + selectResult.rows[x].CUSTNUMBER + "','" + (selectResult.rows[x].CLIENT_NAME).replace(/'/g, '') + "','" + selectResult.rows[x].SECTION + "','" + selectResult.rows[x].OUSTBALANCE + "','" + selectResult.rows[x].TOTALARREARS + "','" + selectResult.rows[x].DAYSINARR + "','" + (selectResult.rows[x].ADDRESS).replace(/'/g, '') + "','" + selectResult.rows[x].POSTALCODE + "','" + selectResult.rows[x].TELNUMBER + "','" + selectResult.rows[x].EMAILADDRESS + "','" + currentDate() + "','" + selectResult.rows[x].COLOFFICER + "','" + selectResult.rows[x].BRANCHCODE + "','" + selectResult.rows[x].RROCODE + "','" + selectResult.rows[x].AROCODE + "','" + selectResult.rows[x].DEMAND + "','" + status + "')"
+
+          insertSQLrs = await connection.execute(insertSQL);
+
+          // invalid emails
+          const status = {
+            id: record.ID,
+            from: 'loans',
+            datesent: currentDate(),
+            status: 'invalid emailaddress',
+            sentby: 'auto'
+          };
+          // push to queue
+
+          amqp.connect(dbConfig.RABBITMQ, (err, conn) => {
+            if (err != null) bail(err);
+            conn.createChannel(on_open);
+            function on_open(err, ch) {
+              if (err != null) bail(err);
+              var queue = 'demandstatus';
+              ch.assertQueue(queue, { durable: false });
+              ch.sendToQueue(queue, Buffer.from(JSON.stringify(status)));
+              console.log('status invalidemail sent to demandstatus queue');
+            }
+          })
+
+        }
+      };
+    };
+    console.log('----End----Demandsdue insert completed on :' + new Date());
+  } catch (err) {
+    console.error(err);
+    console.log('----End err----');
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();   // Always close connections
+      } catch (err) {
+        console.error(err.message);
+        console.log('----End err----');
+      }
+    }
+  }
+})();
+// 
+
+console.log('[' + dbConfig.servicename + '] cronjob service started at ' + new Date());
 
